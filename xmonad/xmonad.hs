2573b4459eb00931300dcb1c84b226066481229c
Louis R. Marascio <lrm@fitnr.com>
2010-07-04 09:56:35 -0500

    Clean up xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,29 +1,33 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import qualified XMonad.StackSet as W
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import Data.List
 import System.IO
 
+import qualified XMonad.StackSet as W
+import qualified Data.Map        as M
 
-main = do
---    xmproc <- spawnPipe "/usr/bin/xmobar /home/lrm/.xmobarrc"
-    xmonad $ defaultConfig
-        { terminal   = myTerminal
-        , workspaces = myWorkspaces
-        , focusFollowsMouse = myFocusFollowsMouse
-        -- , normalBorderColor = myNormalBorderColor
-        -- , focusedBorderColor = myFocusedBorderColor
-        , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
---        , logHook    = dynamicLogWithPP $ xmobarPP
---                           { ppOutput = hPutStrLn xmproc
---                           , ppTitle  = xmobarColor "green" "" . shorten 50
---                           }
-        , modMask    = mod4Mask        -- Rebind Mod to the Windows key
-        } `additionalKeys` myKeys
+-- The preferred terminal program.
+myTerminal = "urxvt"
 
+-- Whether focus follows the mouse pointer.
+myFocusFollowsMouse :: Bool
+myFocusFollowsMouse = False
+
+-- The width and color of the border.
+myBorderWidth        = 1
+myNormalBorderColor  = "#0f0f0f"
+myFocusedBorderColor = "#000044"
+
+-- The modified key we prefer.
+myModMask = mod4Mask
+
+-- The workspaces and their names.
+myWorkspaces = [ "1:wiki", "2:wiki", "3:comm", "4:web", "5:win", "6:",  "9:min" ]
+
+-- Key bindings.
 myKeys = 
     [ ((mod4Mask .|. shiftMask, xK_z),      spawn "xscreensaver-command -lock")
     , ((controlMask,           xK_Print),   spawn "sleep 0.2; scrot -s")
@@ -35,18 +39,33 @@ myKeys =
           , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]
     ]
 
-myTerminal = "urxvt"
-
+-- Window rules. 
 myManageHook = composeAll
-    [ className =? "VirtualBox" --> doFloat
-    , title     =? "wikidiary"  --> doShift "3:wiki"
+    [ (className =? "VirtualBox" <&&> fmap ("robocop" `isPrefixOf`) title) --> (doShift "8:win" <+> unfloat)
+    ,  title     =? "wikidiary" --> doShift "3:wiki"
     ]
+    where
+        unfloat = ask >>= doF . W.sink
 
-myWorkspaces = [ "1:shell", "2:code", "3:wiki", "4:mail", "5:comm", 
-                 "6:web",   "7:vm",   "8:win",  "9:min" ]
+-- Run xmonad.
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/lrm/.xmobarrc"
+    xmonad $ defaultConfig
+        { terminal   = myTerminal
+        , workspaces = myWorkspaces
+        , focusFollowsMouse = myFocusFollowsMouse
+        -- , normalBorderColor = myNormalBorderColor
+        -- , focusedBorderColor = myFocusedBorderColor
+        , manageHook = manageDocks <+> myManageHook <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook    = dynamicLogWithPP $ xmobarPP
+                           { ppOutput = hPutStrLn xmproc
+                           , ppTitle  = xmobarColor "green" "" . shorten 50
+                           }
+        , modMask    = myModMask
+        } `additionalKeys` myKeys
 
-myFocusFollowsMouse = False
 
-myBorderWidth = 1
-myNormalBorderColor = "#0f0f0f"
-myFocusedBorderColor = "#000044"
+
+
+
