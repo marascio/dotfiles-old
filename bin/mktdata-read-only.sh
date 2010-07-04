9174518057fc02dfc19128e8e4d8cfb2cfb8362b
Louis R. Marascio <lrm@fitnr.com>
2010-07-04 12:07:30 -0500

    Add script to keep mkt data perms in check

---
+++ bin/mktdata-read-only.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+MKTDATA="$HOME/mktdata"
+LOG="$HOME/log/mktdata-read-only.log"
+
+files=$(find $MKTDATA -not -regex '.*/\..*' -type f -not -perm ug=r | sort)
+
+for i in $files; do
+    chmod 440 $i
+    echo "Fixed perms for $i" >> $LOG
+done
+
