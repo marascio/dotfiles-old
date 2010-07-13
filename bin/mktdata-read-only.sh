3856265f31a241ffb3d7bb6e1d7403eb8d84842e
Louis R. Marascio <lrm@fitnr.com>
2010-07-12 21:00:14 -0500

    re-enable mktdata read only checker

--- bin/mktdata-read-only.sh
+++ bin/mktdata-read-only.sh
@@ -5,8 +5,8 @@ LOG="$HOME/log/mktdata-read-only.log"
 
 files=$(find $MKTDATA -not -regex '.*/\..*' -type f -not -perm ug=r | sort)
 
-#for i in $files; do
-#    chmod 440 $i
-#    echo "Fixed perms for $i" >> $LOG
-#done
+for i in $files; do
+    chmod 440 $i
+    echo "Fixed perms for $i" >> $LOG
+done
 
