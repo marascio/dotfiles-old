88c72f5678463928d41710602c346c0bfcadb3b6
Louis R. Marascio <lrm@fitnr.com>
2011-05-22 12:39:34 -0500

    update somafm script

--- bin/somafm.sh
+++ bin/somafm.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+[ "$(type -P links)" ] || echo "error: 'links' must be installed" && exit 1
+
 if [ $# -lt 1 ]; then
     echo "error: please specify a stream"
     cat << EOF
@@ -9,6 +11,8 @@ if [ $# -lt 1 ]; then
     poptron        u80s            cliqhop         illstreet
     digitalis      missioncontrol  xmasinfrisko    doomed
     covers         brfm
+
+    Visit: http://somafm.com/ for a list of streams and descriptions
 EOF
     exit 1
 fi
@@ -18,5 +22,8 @@ streamurl="http://somafm.com/startstream=$stream.pls"
 playedurl="http://somafm.com/play/$stream"
 nohup mplayer -really-quiet -vo none -ao alsa $streamurl &>/dev/null &
 pid=$!
-watch -n5 "links -dump $playedurl | sed -n '/Music Director.*/,/SomaFM/p'"
+
+trap 'kill $pid;' TERM QUIT INT
+
+watch -n5 -t "links -dump $playedurl | sed -n '/Music Director.*/,/SomaFM/p'"
 kill $pid
